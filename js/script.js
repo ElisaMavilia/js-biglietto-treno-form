403347fc834bbdd5c292c200b445953d04cd04b5
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-07 18:21:09 +0100

    Add function

--- js/script.js
+++ js/script.js
@@ -36,13 +36,21 @@ else {
     document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${noDiscountedTicket.toFixed(2)} €`;
 } */ 
 
-let elNameSurname = document.getElementById("name-surname");
-let elKilometers = document.getElementById("kilometers");
-let elAge = document.querySelector(".age");
-let elButton1 = document.querySelector(".button-1")
-let elButton2 = document.querySelector(".button-2")
+let elNameSurname = document.getElementById('name');
+let elKilometers = document.getElementById('kilometers');
+let elAge = document.querySelector('.age');
+let elButton1 = document.querySelector('.button-1')
+let elButton2 = document.querySelector('.button-2')
 console.log(elNameSurname, elKilometers, elAge, elButton1, elButton2); 
 
 elButton1.addEventListener('click', function(){ 
-    console.log("hai cliccato il bottone");
+   /*  console.log("hai cliccato il bottone"); */
+    let name = elNameSurname.value;
+    let kilometers = elKilometers.value;
+    let age = elAge.value;
+    console.log(name, kilometers, age);
+ })
+
+ elButton2.addEventListener('click', function(){ 
+   /*  console.log("hai cliccato il bottone"); */
  })
