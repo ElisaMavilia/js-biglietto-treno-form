127c6262d78bd4a4eefd1e238f203aaf59113f2f
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-07 14:33:05 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//TICKET//
+let kilometer = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
+let paxAge = parseInt(prompt("Inserisci la tua età"));
+console.log(kilometer,paxAge);
+
+let ticket = (0.21 * kilometer);
+let noDiscountedTicket = ticket.toFixed(2).replace(/\./, ",");
+console.log(noDiscountedTicket);
+
+// TICKET JUNIOR//
+let discount1 = (ticket * 20) / 100;
+console.log(discount1);
+let discountedTicket1 = (ticket - discount1);
+/* Prezzo finale biglietto 1
+in decimali */
+let finalPrice1 = discountedTicket1;
+console.log(finalPrice1);
+
+// TICKET SENIOR//
+let discount2 = (ticket * 40) / 100;
+console.log(discount2);
+let discountedTicket2 = (ticket - discount2);
+/* Prezzo finale biglietto 2
+in decimali */
+let finalPrice2 = discountedTicket2.toFixed(2).replace(/\./, ",");
+console.log(finalPrice2);
+
+// CONDITION //
+if (paxAge < 18){
+    document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${finalPrice1.toFixed(2)} €`;
+}
+else if (paxAge > 65){
+    document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${finalPrice2.toFixed(2)} €`;
+}
+else {
+    document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${noDiscountedTicket.toFixed(2)} €`;
+}
