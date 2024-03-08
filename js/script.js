1b9a1f1d9a98a8bf4b34909a9044c541926c50a5
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-08 09:19:09 +0100

    Adjustments

--- js/script.js
+++ js/script.js
@@ -1,40 +1,3 @@
-/* //TICKET//
-let kilometer = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
-let paxAge = parseInt(prompt("Inserisci la tua età"));
-console.log(kilometer,paxAge);
-
-let ticket = (0.21 * kilometer);
-let noDiscountedTicket = ticket.toFixed(2).replace(/\./, ",");
-console.log(noDiscountedTicket);
-
-// TICKET JUNIOR//
-let discount1 = (ticket * 20) / 100;
-console.log(discount1);
-let discountedTicket1 = (ticket - discount1);
-/* Prezzo finale biglietto 1
-in decimali */
-/* let finalPrice1 = discountedTicket1;
-console.log(finalPrice1);
-
-// TICKET SENIOR//
-let discount2 = (ticket * 40) / 100;
-console.log(discount2);
-let discountedTicket2 = (ticket - discount2);
-/* Prezzo finale biglietto 2
-in decimali */
-/* let finalPrice2 = discountedTicket2.toFixed(2).replace(/\./, ",");
-console.log(finalPrice2);
-
-// CONDITION //
-if (paxAge < 18){
-    document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${finalPrice1.toFixed(2)} €`;
-}
-else if (paxAge > 65){
-    document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${finalPrice2.toFixed(2)} €`;
-}
-else {
-    document.getElementById(`title`).innerHTML = `Il prezzo del biglietto è: ${noDiscountedTicket.toFixed(2)} €`;
-} */ 
 
 let elNameSurname = document.getElementById('name');
 let elKilometers = document.getElementById('kilometers');
@@ -45,6 +8,8 @@ console.log(elNameSurname, elKilometers, elAge, elButton1, elButton2);
 
 let elSelectedTicket = document.querySelector('.selected-ticket');
 
+let paxAge = parseInt(document.querySelector('.age'));
+
 elButton1.addEventListener('click', function(){ 
    /*  console.log("hai cliccato il bottone"); */
     let name = elNameSurname.value;
@@ -52,9 +17,9 @@ elButton1.addEventListener('click', function(){
     let age = elAge.value;
     console.log(name, kilometers, age);
     elSelectedTicket.classList.remove('d-none');
+    elSelectedTicket.classList.add(age);
+    document.getElementById('pax').innerHTML = name;
     document.getElementById('pax').innerHTML = name;
  })
 
- elButton2.addEventListener('click', function(){ 
-   /*  console.log("hai cliccato il bottone"); */
- })
+
